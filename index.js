030e153cac4165e8d66dd8bf88555261d4665880
jonathanowens2424 <jonathanlouisowens@gmail.com>
2026-01-17 18:34:05 -0600

    Added checks to see if the user submitted a valid number to generate a grid.

--- index.js
+++ index.js
@@ -3,28 +3,34 @@ const changeGridSizeBtn = document.querySelector("#changeGridSizeBtn");
 
 let div = "";
 
-//creates 16x16 grid of blocks
-for (let i = 0; i < 16; i++) {
-  div += `<div class="etchSketchBlock"></div>`;
-}
-for (let i = 0; i < 16; i++) {
-  container.innerHTML += `<div class="etchSketchBlockRow">${div}</div>`;
-}
+function generateGrid(number) {
+  for (let i = 0; i < number; i++) {
+    div += `<div class="etchSketchBlock"></div>`;
+  }
+  for (let i = 0; i < number; i++) {
+    container.innerHTML += `<div class="etchSketchBlockRow">${div}</div>`;
+  }
 
-// on hover, change background color to black
-
-const etchSketchBlocks = document.querySelectorAll(".etchSketchBlock");
-console.log(etchSketchBlocks);
-etchSketchBlocks.forEach((block) => {
-  block.addEventListener("mouseover", () => {
-    block.style.backgroundColor = "black";
+  const etchSketchBlocks = document.querySelectorAll(".etchSketchBlock");
+  console.log(etchSketchBlocks);
+  etchSketchBlocks.forEach((block) => {
+    block.addEventListener("mouseover", () => {
+      block.style.backgroundColor = "black";
+    });
   });
-});
+}
 
-//add Reset Button
-// make black path disentergrate after a while.
+generateGrid(16);
 
 //toggle Grid size feature
 changeGridSizeBtn.addEventListener("click", () => {
   const desiredSize = prompt("What size do you want? Max: 100x100");
+  const sizeNumber = Number(desiredSize);
+  if (sizeNumber > 100) {
+    alert("Your number is too large.");
+  } else if (sizeNumber > 0 && !isNaN(sizeNumber) && sizeNumber !== null) {
+    alert("Generating grid...");
+  } else {
+    alert("Please give a positive number.");
+  }
 });
